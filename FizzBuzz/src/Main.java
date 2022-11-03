d450d28688065e6689aeaa2526562a8ad631f6e3
Dina Dana <dinadana@live.com>
2022-11-03 19:58:21 -0400

    Add files via upload

---
+++ FizzBuzz/src/Main.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 5; i > 0; i--)
+            System.out.println("Hi Dina! " + i);
+
+    }
+}
